eca6fa425c0f429a7ff6a483b3ae9518b8f28f1c
Surhud More <surhud.more@ipmu.jp>
2015-09-16 10:20:31 +0900

    add in docstrings

--- src/py_coda.py
+++ src/py_coda.py
@@ -18,7 +18,28 @@ class IOError(Exception):
 
 class mcmc():
     def __init__(self, labels, data, thin):
+        "Initializes mcmc object
 
+        :Parameters:
+
+        -   labels : An array of strings containing the latex labels for the
+            parameters
+        -   data : An array containing the output of the Monte Carlo Markov
+            chain. The array should have shape (Niterations, Nparam)
+        -   thin : Specify the thinning parameter, this will use only one
+            sample every thin iterations.
+
+        :Returns:
+
+        -   MCMC object
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> help(a)
+
+        "
         from rpy2 import robjects
         from rpy2.robjects.packages import importr
         self.coda = importr("coda")
@@ -42,7 +63,30 @@ class mcmc():
         self.codamcmc = self.coda.mcmc(robjects.r['matrix'](robjects.FloatVector(data.flatten()), nrow=self.chain_elements))
 
     def plot_traces(self, n_at_time=5, backend="TkAgg", savefig=0):
+        "Generate trace plots
 
+        :Parameters:
+
+        -   n_at_time : Optional argument to specify the number of parameters to
+            be plotted on one page, the subplots are arranged in n_at_time rows,
+            with default value equal to 5
+        -   backend : Optional argument to specify the matplotlib backend to be
+            used. By default uses the TkAgg backend.
+        -   savefig : Optional string argument which is appended to the figure
+            names, if non-empty, the plots are not displayed. By default figures
+            are not saved, but just displayed.
+
+        :Returns:
+
+        -   0
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> a.plot_traces(n_at_time=5, backend=\"TkAgg\", savefig=\"MCMC\")
+
+        "
         import matplotlib
         matplotlib.use(backend)
         import pylab as pl
@@ -80,7 +124,24 @@ class mcmc():
         return 0
 
     def get_stats(self, fp=sys.stdout):
+        "Display statistics of the chain, such as the 2.5, 16, 50, 84 and 97.5
+        percentiles
 
+        :Parameters:
+
+        -   fp : Optional python file object, default is sys.stdout
+
+        :Returns:
+
+        -   0
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> a.get_stats()
+
+        "
         sys.stdout.write("Computing stats...\n")
 
         i025 = int(0.025 * self.chain_elements)
@@ -101,6 +162,30 @@ class mcmc():
             fp.write('\t'.join([x for x in list_to_print])+"\n")
 
     def plot_autocorr(self, n_at_time=6, backend="TkAgg", savefig=0):
+        "Generate plots with autocorrelations
+
+        :Parameters:
+
+        -   n_at_time : Optional argument to specify the number of parameters to
+            be plotted on one page, the subplots are arranged in n_at_time rows,
+            with default value equal to 5
+        -   backend : Optional argument to specify the matplotlib backend to be
+            used. By default uses the TkAgg backend.
+        -   savefig : Optional string argument which is appended to the figure
+            names, if non-empty, the plots are not displayed. By default figures
+            are not saved, but just displayed.
+
+        :Returns:
+
+        -   0
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> a.plot_autocorr(n_at_time=5, backend=\"TkAgg\", savefig=\"MCMC\")
+
+        "
 
         import matplotlib
         matplotlib.use(backend)
@@ -139,15 +224,62 @@ class mcmc():
 
         return 0
 
-    def heidelberger_welch(self, eps=0.1, pvalue=0.05):
+    def heidelberger_welch(self, pvalue=0.05, eps=0.1):
+        "Performs the Heidelberger Welch test for stationarity and half width.
+
+        :Parameters:
+
+        -   pvalue : Optional pvalue to be used to decide the success or failure
+            of the stationarity test (default=0.05)
+        -   eps : Optional argument which sets a tolerance level to decide the
+            success of the halfwidth test (default=0.1)
+
+        :Returns:
+
+        -   Prints diagnostic text output, can optionally return the
+            quantitative results of the test in an array as well
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> a.heidelberger_welch()
+
+        "
         hw_output = self.coda.heidel_diag(self.codamcmc, eps=eps, pvalue=pvalue)
         sys.stdout.write("Computing Heidelberger Welch stationarity and the half width test, please be patient...\n")
 
         print hw_output
-        return 0
+        return hw_output
 
     def geweke(self, nbins=20, n_at_time=6, backend="TkAgg", savefig=0):
+        "Plots the z-scores of the Geweke test
 
+        :Parameters:
+
+        -   nbins : Optional argument which sets the number of different
+            starting points in the chain to use to perform the Geweke test
+            (default=20)
+        -   n_at_time : Optional argument to specify the number of parameters to
+            be plotted on one page, the subplots are arranged in n_at_time rows,
+            with default value equal to 5
+        -   backend : Optional argument to specify the matplotlib backend to be
+            used. By default uses the TkAgg backend.
+        -   savefig : Optional string argument which is appended to the figure
+            names, if non-empty, the plots are not displayed. By default figures
+            are not saved, but just displayed.
+
+        :Returns:
+
+        -   Generates plots with Geweke z-scores
+
+        :Examples:
+
+        >>> from py_coda import mcmc
+        >>> a = mcmc(label_array, mcmc_array, 1)
+        >>> a.geweke()
+
+        "
         import matplotlib
         matplotlib.use(backend)
         import pylab as pl
@@ -188,10 +320,32 @@ class mcmc():
         return 0
 
 
-'''
-Routine to read native pycoda format, designed with my chainfiles in mind
-'''
 def read_pycoda(chainfile, indexfile, thin=1, **kwargs):
+    "Reads in flat text file MCMC output
+
+    :Parameters:
+
+    -   chainfile : Name of the chainfile to be read. Should contain the
+        parameters of the model at every step on every line.
+    -   indexfile : Name of the file that contains latex labels for every
+        parameter
+    -   thin : Optional argument to specify the thinning interval (default=1)
+    -   **kwargs : Optional keyword arguments that can be passed to the
+        numpy.loadtxt command for reading the chainfile (e.g., read only
+        specific columns using usecols)
+
+    :Returns:
+
+    -   An py_coda mcmc object
+
+    :Examples:
+
+    >>> from py_coda import read_pycoda
+    >>> a = read_pycoda(\"chainfile.out\", \"chainfile.labels\", thin=1,
+    usecols=(0, 1, 2))
+    >>> help(a)
+
+    "
     labels = [line.rstrip("\n") for line in open(indexfile)]
 
     data = np.loadtxt(chainfile, **kwargs)
